80b9d34a6606920ace9da7fbabfebc3694e4d863
Sunil D N <sunil@Sunils-MacBook-Pro.local>
2022-04-09 00:40:58 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import Header from "./components/common/Header"
+import screenschot from "./sunil.png"
 
 function App() {
-  return <Header/>;
+  return <div onClick={()=> {
+    alert("my nbame is sunil")
+  }}> sunbil
+  <img src={"as"}  style ={{ width: "200px", height : "200px"}} alt={"asdadsd"}/>
+  </div>;
 }
 
 export default App;
